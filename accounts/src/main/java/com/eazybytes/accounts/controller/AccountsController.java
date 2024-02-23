6cb97c491cfab0c2b93d193cfbe4be26a82061bc
Rexxay <rexxay@Rexxays-Mac-mini.local>
2024-02-23 11:02:13 -0800

    Initialize the project.

---
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AccountsController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
