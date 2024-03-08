6385deb452775a00bd5ddec8f56bdfd860da593f
Rijkstarhn <841206675@qq.com>
2024-03-07 16:51:54 -0800

    Added the log tracing of corelation id in account service.

--- accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -10,13 +10,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -27,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 )
 @Validated
 public class CustomerController {
+    private final static Logger logger = LoggerFactory.getLogger(CustomerController.class);
     private final ICustomersService iAccountsService;
 
     @Operation(
@@ -48,10 +48,12 @@ public class CustomerController {
     }
     )
     @GetMapping("/fetchCustomerDetails")
-    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
+                                                                   @RequestParam
                                                                    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        CustomerDetailsDto customerDetailsDto = iAccountsService.fetchCustomerDetails(mobileNumber);
+        logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+        CustomerDetailsDto customerDetailsDto = iAccountsService.fetchCustomerDetails(correlationId, mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
     }
 }
