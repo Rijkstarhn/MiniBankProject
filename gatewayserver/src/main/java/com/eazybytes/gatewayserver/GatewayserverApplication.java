7f4797bbdbdc336c2319f71c6b81c3675fd85106
Rijkstarhn <841206675@qq.com>
2024-03-12 09:46:24 -0700

    Added the gateway server level retry.

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -28,7 +30,11 @@ public class GatewayserverApplication {
 								.uri("lb://ACCOUNTS"))
 				.route("card service",
 						r -> r.path("/eazybank/cards/**")
-								.filters(f -> f.stripPrefix(2).addResponseHeader("responseTime", LocalDateTime.now().toString()))
+								.filters(f -> f.stripPrefix(2)
+										.addResponseHeader("responseTime", LocalDateTime.now().toString())
+										.retry(retryConfig -> retryConfig.setRetries(3).
+												setMethods(HttpMethod.GET)
+												.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
 								.uri("lb://CARDS"))
 				.route("loan service",
 						r -> r.path("/eazybank/loans/**")
